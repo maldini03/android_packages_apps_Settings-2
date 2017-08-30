387be8bb5592e486ea3cb5e8b1d56cd34784e496
maxwen <max.weninger@gmail.com>
2017-08-31 00:30:09 +0200

    Add BananaGear dashboard entry
    Signed-off-by: Rizky Benggolo <travarilo@gmail.com>
    Change-Id: I60891cd1063eac053d12bd9b5fa327ae8075fb8e

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -436,4 +436,8 @@ public class Settings extends SettingsActivity {
      * Activity for OneHandedSettings
      */
     public static class OneHandedSettingsActivity extends SettingsActivity { /* empty */ }
+
+    // BananaDroid
+    public static class BananaGearActivity extends SettingsActivity {}
+
 }
