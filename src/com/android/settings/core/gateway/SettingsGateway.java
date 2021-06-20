Havoc-OS/android_packages_apps_Settings

Settings: Allow to quickly open Running Services from QS panel [2/2]

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -36,6 +36,7 @@ import com.android.settings.accounts.ManagedProfileSettings;
 import com.android.settings.applications.AppAndNotificationDashboardFragment;
 import com.android.settings.applications.ProcessStatsSummary;
 import com.android.settings.applications.ProcessStatsUi;
+import com.android.settings.applications.RunningServices;
 import com.android.settings.applications.UsageAccessDetails;
 import com.android.settings.applications.appinfo.AppInfoDashboardFragment;
 import com.android.settings.applications.appinfo.DrawOverlayDetails;
@@ -291,6 +292,7 @@ public class SettingsGateway {
             ConfigCenter.class.getName(),
             GesturesSettings.class.getName(),
             GamingMode.class.getName(),
+            RunningServices.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -331,5 +333,6 @@ public class SettingsGateway {
             UserBackupSettingsActivity.class.getName(),
             Settings.ConfigCenterActivity.class.getName(),
             Settings.GamingModeActivity.class.getName(),
+            Settings.DevRunningServicesActivity.class.getName(),
     };
 }

maldini03/android_packages_apps_Settings-2

Change-Id: Iccad88a40a38a20e8b60662c38f57d814da32753

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -36,6 +36,7 @@ import com.android.settings.accounts.ManagedProfileSettings;
 import com.android.settings.applications.AppAndNotificationDashboardFragment;
 import com.android.settings.applications.ProcessStatsSummary;
 import com.android.settings.applications.ProcessStatsUi;
+import com.android.settings.applications.RunningServices;
 import com.android.settings.applications.UsageAccessDetails;
 import com.android.settings.applications.appinfo.AppInfoDashboardFragment;
 import com.android.settings.applications.appinfo.DrawOverlayDetails;
@@ -312,7 +313,8 @@ public class SettingsGateway {
             InteractAcrossProfilesDetails.class.getName(),
             MediaControlsSettings.class.getName(),
             BananaGear.class.getName(),
-            SmartPixels.class.getName()
+            SmartPixels.class.getName(),
+            RunningServices.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -355,5 +357,6 @@ public class SettingsGateway {
             // BananaDroid
             Settings.BananaGearActivity.class.getName(),
             Settings.SmartPixelsActivity.class.getName(),
+            Settings.DevRunningServicesActivity.class.getName(),
     };
 }