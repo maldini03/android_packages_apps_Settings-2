syberia-project/platform_packages_apps_Settings

Settings: add back weather parts [2/3]
used from e.g. qs weather tile

Change-Id: Ifa2a161c7687ee1825d2f7e0689cdad174d1f8e7
Signed-off-by: mydongistiny <jaysonedson@gmail.com>
Signed-off-by: DennySPB <dennyspb@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -138,6 +138,7 @@ import com.android.settings.wifi.calling.WifiCallingSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 
 import com.syberia.settings.SyberiaSettings;
+import com.syberia.settings.fragments.OmniJawsSettings;
 
 public class SettingsGateway {
 
@@ -264,6 +265,7 @@ public class SettingsGateway {
             ToggleBackupSettingFragment.class.getName(),
             PreviouslyConnectedDeviceDashboardFragment.class.getName(),
             SyberiaSettings.class.getName(),
+            OmniJawsSettings.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -303,5 +305,6 @@ public class SettingsGateway {
             Settings.EnterprisePrivacySettingsActivity.class.getName(),
             Settings.MyDeviceInfoActivity.class.getName(),
             Settings.SyberiaSettingsActivity.class.getName(),
+            Settings.OmniJawsSettingsActivity.class.getName(),
     };
 }

maldini03/android_packages_apps_Settings-2

Change-Id: Icde0d6fb9c70fbbf9f245e4961f329021e14f270

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -165,6 +165,7 @@ import com.android.settings.wifi.tether.WifiTetherSettings;
 import com.banana.settings.BananaGear;
 import com.banana.settings.fragments.GamingMode;
 import com.banana.settings.fragments.SmartPixels;
+import com.banana.settings.fragments.OmniJawsSettings;
 
 public class SettingsGateway {
 
@@ -318,7 +319,8 @@ public class SettingsGateway {
             BananaGear.class.getName(),
             SmartPixels.class.getName(),
             RunningServices.class.getName(),
-            GamingMode.class.getName()
+            GamingMode.class.getName(),
+            OmniJawsSettings.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -363,5 +365,6 @@ public class SettingsGateway {
             Settings.SmartPixelsActivity.class.getName(),
             Settings.DevRunningServicesActivity.class.getName(),
             Settings.GamingModeActivity.class.getName(),
+            Settings.OmniJawsSettingsActivity.class.getName(),
     };
 }