748a0ecf29b6cdbd01ba1bdd960b320e56f8b5b8
vjspranav <pranavasri@live.in>
2022-07-13 01:47:39 +0200

    Settings: Add toggle for Custom UI [2/3]
    Change-Id: I541f18ff25280f7b117aeb949a14f2efc1ab5b5c
    Signed-off-by: aswin7469 <aswinas@pixysos.com>

--- src/com/android/settings/homepage/TopLevelSettings.java
+++ src/com/android/settings/homepage/TopLevelSettings.java
@@ -16,6 +16,7 @@
 
 package com.android.settings.homepage;
 
+import static android.provider.Settings.System.CUSTOM_UI_TOGGLE;
 import static com.android.settings.search.actionbar.SearchMenuController.NEED_SEARCH_ICON_IN_ACTION_BAR;
 import static com.android.settingslib.search.SearchIndexable.MOBILE;
 
@@ -28,6 +29,7 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.UserHandle;
 import android.provider.Settings;
 import android.text.TextUtils;
 import android.util.Log;
@@ -200,7 +202,11 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
                 icon.setTint(tintColor);
             }
         });
-        onSetPrefCard();
+        boolean customUIToggleValue = Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.CUSTOM_UI_TOGGLE, 0, UserHandle.USER_CURRENT) != 0;
+        if (customUIToggleValue) {
+            onSetPrefCard();
+        }
     }
 
     private void onSetPrefCard() {
