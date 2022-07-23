Hyper-Paranoid/android_packages_apps_Settings

Add BananaGear dashboard entry
xlxfoxxlx:
-update to P, add oval background
-update to ten
-update to eleven

Change-Id: Iaf927e8fef982681e6d485ea4e878b6c988a6596
Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>
Signed-off-by: Rizky Benggolo <travarilo@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -160,6 +160,8 @@ import com.android.settings.wifi.savedaccesspoints.SavedAccessPointsWifiSettings
 import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSettings2;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
+import com.banana.settings.BananaGear;
+
 public class SettingsGateway {
 
     /**
@@ -307,7 +309,8 @@ public class SettingsGateway {
             GestureNavigationSettingsFragment.class.getName(),
             InteractAcrossProfilesSettings.class.getName(),
             InteractAcrossProfilesDetails.class.getName(),
-            MediaControlsSettings.class.getName()
+            MediaControlsSettings.class.getName(),
+            BananaGear.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -347,5 +350,7 @@ public class SettingsGateway {
             Settings.MyDeviceInfoActivity.class.getName(),
             Settings.ModuleLicensesActivity.class.getName(),
             UserBackupSettingsActivity.class.getName(),
+            // BananaDroid
+            Settings.BananaGearActivity.class.getName(),
     };
 }

maldini03/android_packages_apps_Settings-2

Change-Id: I60891cd1063eac053d12bd9b5fa327ae8075fb8e

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -168,6 +168,8 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSettings2;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
+import com.banana.settings.BananaGear;
+
 public class SettingsGateway {
 
     /**
@@ -323,7 +325,8 @@ public class SettingsGateway {
             NetworkProviderSettings.class.getName(),
             NetworkSelectSettings.class.getName(),
             AlarmsAndRemindersDetails.class.getName(),
-            MediaManagementAppsDetails.class.getName()
+            MediaManagementAppsDetails.class.getName(),
+            BananaGear.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -364,5 +367,7 @@ public class SettingsGateway {
             Settings.MyDeviceInfoActivity.class.getName(),
             Settings.ModuleLicensesActivity.class.getName(),
             UserBackupSettingsActivity.class.getName(),
+            // BananaDroid
+            Settings.BananaGearActivity.class.getName(),
     };
 }