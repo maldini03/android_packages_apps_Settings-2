Evolution-X-AOSP/packages_apps_Settings

Settings: Remove LiveDisplay if not supported
Change-Id: I0032b88e1fcaf918935b861434c8b4a4cf090cd9
Signed-off-by: Joey Huab <joey@evolution-x.org>

src/com/android/settings/display/LiveDisplayPreferenceController.java
@@ -1,0 +1,51 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.android.settings.display;
+
+import android.content.Context;
+
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settings.R;
+
+public class LiveDisplayPreferenceController extends AbstractPreferenceController implements
+        PreferenceControllerMixin {
+
+    private static final String KEY_LIVE_DISPLAY = "livedisplay";
+
+    public LiveDisplayPreferenceController(Context context) {
+        super(context);
+    }
+
+    public static boolean isSuggestionComplete(Context context) {
+        final boolean isEnabled = context.getResources().getBoolean(
+                com.android.internal.R.bool.config_liveDisplayAvailable);
+        // The suggestion is always complete if not enabled.
+        if (!isEnabled) {
+            return true;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_liveDisplayAvailable);
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_LIVE_DISPLAY;
+    }
+}

maldini03/android_packages_apps_Settings-2

src/com/android/settings/display/LiveDisplayPreferenceController.java
@@ -1,0 +1,51 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+package com.android.settings.display;
+
+import android.content.Context;
+
+import com.android.settings.core.PreferenceControllerMixin;
+import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settings.R;
+
+public class LiveDisplayPreferenceController extends AbstractPreferenceController implements
+        PreferenceControllerMixin {
+
+    private static final String KEY_LIVE_DISPLAY = "livedisplay";
+
+    public LiveDisplayPreferenceController(Context context) {
+        super(context);
+    }
+
+    public static boolean isSuggestionComplete(Context context) {
+        final boolean isEnabled = context.getResources().getBoolean(
+                com.android.internal.R.bool.config_enableLiveDisplay);
+        // The suggestion is always complete if not enabled.
+        if (!isEnabled) {
+            return true;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return mContext.getResources().getBoolean(
+                com.android.internal.R.bool.config_enableLiveDisplay);
+    }
+
+    @Override
+    public String getPreferenceKey() {
+        return KEY_LIVE_DISPLAY;
+    }
+}