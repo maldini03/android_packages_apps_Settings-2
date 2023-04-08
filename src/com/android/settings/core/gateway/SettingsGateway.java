crdroidandroid/android_packages_apps_Settings

Fix long click intent for Smart Pixels tile [2/2]
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -150,6 +150,7 @@ import com.crdroid.settings.fragments.navigation.PieSettings;
 import com.crdroid.settings.fragments.navigation.smartnav.SmartbarSettings;
 import com.crdroid.settings.fragments.navigation.smartnav.FlingSettings;
 import com.crdroid.settings.fragments.navigation.smartnav.PulseSettings;
+import com.crdroid.settings.fragments.ui.SmartPixels;
 
 public class SettingsGateway {
 
@@ -277,6 +278,7 @@ public class SettingsGateway {
             ToggleBackupSettingFragment.class.getName(),
             PreviouslyConnectedDeviceDashboardFragment.class.getName(),
             crDroidSettingsLayout.class.getName(),
+            SmartPixels.class.getName(),
             SmartbarSettings.class.getName(),
             FlingSettings.class.getName(),
             GamingMode.class.getName(),
@@ -322,6 +324,7 @@ public class SettingsGateway {
             Settings.EnterprisePrivacySettingsActivity.class.getName(),
             Settings.MyDeviceInfoActivity.class.getName(),
             Settings.crDroidSettingsLayoutActivity.class.getName(),
+            Settings.SmartPixelsActivity.class.getName(),
             Settings.SmartbarSettingsActivity.class.getName(),
             Settings.FlingSettingsActivity.class.getName(),
             Settings.GamingModeActivity.class.getName(),

maldini03/android_packages_apps_Settings-2

Change-Id: Id6b5fffd814f035be1bb75d1434456e0c02aa97f

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -181,6 +181,7 @@ import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSetting
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
 import com.banana.settings.BananaGear;
+import com.banana.settings.fragments.ui.SmartPixels;
 
 public class SettingsGateway {
 
@@ -350,7 +351,8 @@ public class SettingsGateway {
             TurnScreenOnSettings.class.getName(),
             TurnScreenOnDetails.class.getName(),
             BananaGear.class.getName(),
-            LiveDisplaySettings.class.getName()
+            LiveDisplaySettings.class.getName(),
+            SmartPixels.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -395,5 +397,6 @@ public class SettingsGateway {
             UserBackupSettingsActivity.class.getName(),
             // BananaDroid
             Settings.BananaGearActivity.class.getName(),
+            Settings.SmartPixelsActivity.class.getName(),
     };
 }